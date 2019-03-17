6c659693e21d6d8d3937841977d6cff8165cad9d
khrys63 <khrys63@gmail.com>
2019-03-17 14:52:43 +0100

    check sec

--- include/dbconf.php
+++ include/dbconf.php
@@ -1,13 +1,16 @@
 <?php
+if (!defined("LOADED_AS_MODULE")) {
+    die ("Vous n'&ecirc;tes pas autoris&eacute; &agrave; acc&eacute;der directement &agrave; cette page...");
+}
 if ($GLOBALS['DEBUG']==0) {
-    $config['localhost']= "localhost";
-    $config['dbName'] 	= "copie_ovh";
-    $config['login'] 	= "root";
-    $config['pwd'] 		= "";
+    $config['localhost']    = "prod"; 
+    $config['dbName']       = "dbprod";
+    $config['login']        = "dblogin";
+    $config['pwd']          = "dbpass";
 } else {
-    $config['localhost']= "localhost";
-    $config['dbName'] 	= "copie_ovh";
-    $config['login'] 	= "root";
-    $config['pwd'] 		= "";
+    $config['localhost']    = "localhost";
+    $config['dbName']       = "copy_prod";
+    $config['login']        = "root";
+    $config['pwd']          = "";
 }
 ?>
